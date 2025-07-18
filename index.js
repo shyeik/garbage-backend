97910806094ce98909c625e9ad577ad8e802a7b6
Jake Derilo <derilojake2528@gmail.com>
2025-07-19 01:38:17 +0800

    hello word

--- index.js
+++ index.js
@@ -27,3 +27,7 @@ app.use("/api/announcements", announcementRoutes);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello world!" });
+});
